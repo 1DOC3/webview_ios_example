6c59bade38fa11c550172e9e7e77ee816dbdd6e0
Pablo Chaves <pablo161096@gmail.com>
2023-10-10 17:50:24 -0500

    add configuration to download files

--- Webview_IOS/ContentView.swift
+++ Webview_IOS/ContentView.swift
@@ -1,6 +1,7 @@
 
 import SwiftUI
 import WebKit
+import QuickLook
 
 struct ContentView: View {
     var body: some View {
@@ -23,7 +24,7 @@ struct WebViewContainer: UIViewRepresentable {
         webViewConf.preferences = webViewPrefs
         webViewConf.allowsInlineMediaPlayback = true // Configuración que evita pantalla de video en negro
         webViewConf.mediaTypesRequiringUserActionForPlayback = []
-        
+                
         let webView = WKWebView(frame: CGRect.zero, configuration: webViewConf)
         webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         webView.scrollView.keyboardDismissMode = .onDrag
@@ -57,7 +58,116 @@ struct WebViewContainer: UIViewRepresentable {
         
         // WKNavigationDelegate - Manejo de eventos de navegación
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            // Aquí podrías realizar acciones cuando la página se ha cargado completamente
+        // Aquí podrías realizar acciones cuando la página se ha cargado completamente
+        }
+            
+        // WKNavigationDelegate - Manejo de eventos de navegación
+        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+            
+            if let url = navigationAction.request.url {
+                // Comprobar si la URL de navegación es un archivo que deseas descargar
+                if isDownloadableFile(url) {
+                    // Aquí puedes manejar la descarga
+                    downloadFileInExternalBrowser(url)
+                    
+                    // Evitar la navegación adicional para evitar que el archivo se abra en el WebView
+                    decisionHandler(.cancel)
+                    return
+                }
+                
+                if isRedirectDownloadableFile(url) {
+                    let cookieStore = webView.configuration.websiteDataStore.httpCookieStore
+                    // Obtener todas las cookies del WKWebView
+                    cookieStore.getAllCookies { cookies in
+                        for cookie in cookies {
+                            // Asignar el nombre y el valor de la cookie
+                            HTTPCookieStorage.shared.setCookie(cookie)
+                        }
+                    }
+                    
+                    var request = URLRequest(url: url)
+
+                    let cookies = HTTPCookieStorage.shared.cookies(for: url)!
+                    let headers = HTTPCookie.requestHeaderFields(with: cookies)
+
+                    request.allHTTPHeaderFields = headers
+                    
+                    // Crear una tarea de datos para la solicitud GET
+                    let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                        if let error = error {
+                            print("Error: \(error)")
+                        } else if  let httpResponse = response as? HTTPURLResponse{
+                            if httpResponse.statusCode == 200, let redirectURL = httpResponse.url {
+                                self.downloadFileInExternalBrowser(redirectURL)
+                               
+                            } else {
+                               // La respuesta no es una redirección o no contiene la URL de redirección
+                               let str = String(data: data!, encoding: .utf8)
+                               print("Received data:\n\(str ?? "")")
+                           }
+                        }
+                    }
+
+                    task.resume()
+                    
+                    decisionHandler(.cancel)
+                    return
+                }
+            }
+            // Permitir la navegación normal en otras URL
+            decisionHandler(.allow)
+        }
+        
+        // Función para verificar si la URL es un archivo descargable (personaliza según tus necesidades)
+        func isDownloadableFile(_ url: URL) -> Bool {
+            // Aquí puedes implementar tu lógica para determinar si la URL es un archivo descargable
+            // Por ejemplo, puedes verificar la extensión del archivo o cualquier otro criterio
+            // Devuelve true si es un archivo descargable, false en caso contrario
+            
+            let fileExtension = url.pathExtension.lowercased()
+            let allowedExtensions = ["pdf", "jpeg", "jpg", "png"]
+            return allowedExtensions.contains(fileExtension)
+        }
+        
+        func isRedirectDownloadableFile(_ url: URL) -> Bool {
+            // Aquí puedes implementar tu lógica para determinar si la URL es un archivo descargable
+            // Por ejemplo, puedes verificar la extensión del archivo o cualquier otro criterio
+            // Devuelve true si es un archivo descargable, false en caso contrario
+            
+            // Obtener la extensión del archivo desde la URL
+            if let queryItems = URLComponents(url: url, resolvingAgainstBaseURL: false)?.queryItems {
+                for queryItem in queryItems {
+                    if queryItem.name == "name" {
+                        if let fileName = queryItem.value {
+                            print("Nombre del archivo: \(fileName)")
+                            
+                            let components = fileName.components(separatedBy: ".")
+                            let fileExtension = components.last
+                            
+                            // Verificar si la extensión es una de las admitidas
+                            let allowedExtensions = ["pdf", "jpeg", "jpg", "png"]
+                            return allowedExtensions.contains(fileExtension!)
+                        }
+                        
+                    }
+                }
+            }
+    
+            return false
+        }
+        
+        func downloadFileInExternalBrowser(_ url: URL) {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:]) { success in
+                    if success {
+                        print("Abriendo en el navegador externo...")
+                    } else {
+                        print("No se pudo abrir en el navegador externo")
+                    }
+                }
+            } else {
+                print("No se puede abrir la URL en el navegador externo")
+            }
         }
     }
 }
